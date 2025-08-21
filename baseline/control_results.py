2a8e2fbbe8d9f751bfd43ad919ad7fad0fcea127
robolab <pilar.bachiller@gmail.com>
2025-08-21 19:38:58 +0200

    add args to control results

--- baseline/control_results.py
+++ baseline/control_results.py
@@ -1,21 +1,127 @@
+import sys
+import os
+import torch
+from torch.utils.data import DataLoader
+from torch.nn import functional as F
 import numpy as np
-
+from rnn import RNNModel
+from dataset_rnn import TrajectoryDataset, collate_fn
+import json
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from matplotlib.patches import Patch
 from matplotlib.legend_handler import HandlerTuple
+import argparse
+
+parser = argparse.ArgumentParser(prog='control results',
+                    description='Evaluate the specified model againts the control questions.')
+parser.add_argument('--model', type=str, nargs="?", required = True, help="Model used for rating prediction.")
+parser.add_argument('--context', type=str, nargs="?", required = True,  help="Context file for the generation of the context variables.")
+parser.add_argument('--control_path', type=str, nargs="?", default='.', help="Path to the labeled control trajectories.")
+
+args = parser.parse_args()
+
+checkpoint_path = args.model
+print(checkpoint_path)
+model_name = checkpoint_path
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load the checkpoint
+checkpoint = torch.load(checkpoint_path, map_location=device)
+
+input_size = checkpoint['input_size']
+hidden_size = checkpoint['hidden_size']
+num_layers = checkpoint['num_layers']
+
+if 'use_new_context' in checkpoint.keys():
+    USE_NEW_CONTEXT = checkpoint['use_new_context']
+else:
+    USE_NEW_CONTEXT = False
+if 'rnn_type' in checkpoint.keys():
+    RNN_TYPE = checkpoint['rnn_type']
+else:
+    RNN_TYPE = 'GRU'
+if 'frame_threshold' in checkpoint.keys():
+    FRAME_THRESHOLD = checkpoint['frame_threshold']
+else:
+    FRAME_THRESHOLD = 0.1
+
+if 'linear_layers' in checkpoint.keys():
+    LINEAR_LAYERS = checkpoint['linear_layers']
+else:
+    LINEAR_LAYERS = []
+
+if 'activation' in checkpoint.keys():
+    ACTIVATION = checkpoint['activation']
+else:
+    ACTIVATION = 'linear'
+
+if 'context_features' in checkpoint.keys():
+    CONTEXT_FEATURES = checkpoint['context_features']
+else:
+    CONTEXT_FEATURES = 0
 
 
-means_o     = [ 0.3967, 0.4113, 0.7461, 0.7908, 0.7647, 0.6498, 0.6939, 0.5688, 0.8220, 0.8638, 0.5957, 0.5780, 0.8292, 0.6869, 0.9241 ]
-stddevs     = [ 0.2015, 0.2758, 0.2320, 0.1476, 0.1735, 0.2715, 0.2001, 0.2382, 0.1106, 0.1519, 0.2785, 0.3006, 0.2317, 0.2344, 0.1561 ]
-predictions = [ 0.4691, 0.4890, 0.8708, 0.7450, 0.7656, 0.6820, 0.7519, 0.6025, 0.7352, 0.8248, 0.7528, 0.6426, 0.8452, 0.8235, 0.8787]
+model = RNNModel(input_size, hidden_size, num_layers, rnn_type = RNN_TYPE, linear_layers = LINEAR_LAYERS, activation=ACTIVATION, context_vars=CONTEXT_FEATURES).to(device)
+    
+model = model.to(device)
 
 
-x = [x for x in range(len(means_o))]
+model.load_state_dict(checkpoint['model_state_dict'], strict=True)
 
-means, predictions = zip(*sorted(zip(means_o, predictions)))
-means, x_sort,     = zip(*sorted(zip(means_o, x)))
-means, stddevs     = zip(*sorted(zip(means_o, stddevs)))
+controldir = args.control_path
+contextQ_file = args.context
+
+control_indices = [3007,2007,1007,7,302,1302,2302,3102,2002,1002,2,3094,2894,1879,834]
+control_indices.sort()
+
+control_scenarios = dict()
+evaluation_trajectories = list()
+file_list = os.listdir(controldir)
+file_list.sort()
+for filename in file_list:
+    scenario = int(filename.split('_')[0])
+    filepath = os.path.join(controldir, filename)
+    if scenario not in control_scenarios.keys():
+        control_scenarios[scenario] = []
+        evaluation_trajectories.append(filepath)
+    with open(filepath, 'r') as traj_file:
+        trajectory_data = json.load(traj_file)
+    control_scenarios[scenario].append(trajectory_data['label'])
+
+
+mean_control = list()
+median_control = list()
+std_control = list()
+for s in control_scenarios:
+    mean = np.mean(control_scenarios[s])
+    median = np.median(control_scenarios[s])
+    std = np.std(control_scenarios[s])
+    mean_control.append(mean)
+    median_control.append(median)
+    std_control.append(std)
+
+dataset = TrajectoryDataset(evaluation_trajectories, contextQ_file, path = '.', limit=-1, frame_threshold=FRAME_THRESHOLD, data_augmentation=False, reload = False)
+loader = DataLoader(dataset,batch_size=32, shuffle=False, collate_fn=collate_fn)
+
+model.eval()
+loss_mse = 0
+loss_mae = 0
+with torch.no_grad():
+    for val_data in loader:
+        traj_batch, label_batch, seq_length = val_data
+        traj_batch = traj_batch.to(device)
+        seq_length = seq_length.to(device)
+
+        predictions = model(traj_batch, seq_length).squeeze()
+        # print('predictions', predictions.tolist())
+predictions = predictions.tolist()
+
+x = [x for x in range(len(mean_control))]
+
+means, predictions = zip(*sorted(zip(mean_control, predictions)))
+means, x_sort,     = zip(*sorted(zip(mean_control, x)))
+means, std_control     = zip(*sorted(zip(mean_control, std_control)))
 
 errors = [p-m for m,p in zip(means,predictions)]
 errorsP = [ [0,          max(0, e)] for e in errors ]
@@ -67,7 +173,7 @@ plt.ylabel("score")
 
 
 plt.axhline(y=1, color='gray', linestyle='-', linewidth=1, zorder=1)
-plt.errorbar(x, means, yerr=stddevs,   fmt='s', color='black', ecolor='black', elinewidth=1, capsize=3, ms=4.5,  zorder=2)
+plt.errorbar(x, means, yerr=std_control,   fmt='s', color='black', ecolor='black', elinewidth=1, capsize=3, ms=4.5,  zorder=2)
 # plt.scatter(x, predictions, facecolors=None,  zorder=3)
 
 LW=1.1
